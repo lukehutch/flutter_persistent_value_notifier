6ed315d1a2108f634c3883192be1476512d9d828
Luke Hutchison <luke.hutch@gmail.com>
2023-10-04 15:09:01 -0600

    Allow value to be read before SharedPreferences has initialized

--- lib/src/persistent_value_notifier.dart
+++ lib/src/persistent_value_notifier.dart
@@ -12,17 +12,34 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-SharedPreferences? _sharedPreferences;
+/// Stores the SharedPreferences instance once it is initialized.
+final _sharedPreferencesNotifier = ValueNotifier<SharedPreferences?>(null)
+  ..addListener(() {});
 
 /// Initialize [SharedPreferences] for [PersistentValueNotifier],
 /// and read initial persisted values
 Future<void> initPersistentValueNotifier() async =>
-    _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferencesNotifier.value = await SharedPreferences.getInstance();
 
 /// A nullable [ValueNotifier] that persists value changes by using
 /// [SharedPreferences] as a write-through cache.
 class PersistentValueNotifier<T> extends ValueNotifier<T> {
+  /// The key to use when storing in SharedPreferences.
   final String sharedPreferencesKey;
+
+  /// Function that is run when SharedPreferences is initialized, if it's not
+  /// initialized yet when the constructor is run. Updates the value.
+  void Function()? _updateToLatestValue;
+
+  /// Run when SharedPreferences is initialized, if it's not initialized yet
+  /// when the constructor is run
+  void _onSharedPreferencesInit() {
+    if (_updateToLatestValue != null) {
+      _updateToLatestValue!();
+      _updateToLatestValue = null;
+    }
+    _sharedPreferencesNotifier.removeListener(_onSharedPreferencesInit);
+  }
 
   /// A nullable [ValueNotifier] backed by [SharedPreferences].
   /// [sharedPreferencesKey] specifies the key to use when storing the value
@@ -31,40 +48,75 @@ class PersistentValueNotifier<T> extends ValueNotifier<T> {
   PersistentValueNotifier(
       {required this.sharedPreferencesKey, required T initialValue})
       : super(initialValue) {
-    assert(_sharedPreferences != null,
-        'Need to call `await initPersistentValueNotifier()` first');
+    if (_sharedPreferencesNotifier.value == null) {
+      // SharedPreferences is not initialized yet -- delay update for when
+      // it is initialized
+      _updateToLatestValue =
+          () => _readFromSharedPreferencesAndSetValue(initialValue);
+      // Add listner to detect when SharedPreferences is initialized
+      _sharedPreferencesNotifier.addListener(_onSharedPreferencesInit);
+    } else {
+      // Initialize value right now from SharedPreferences, or from initialValue
+      // if SharedPreferences doesn't have a value for this key yet
+      _readFromSharedPreferencesAndSetValue(initialValue);
+    }
+  }
+
+  @override
+  set value(T newValue) {
+    if (_sharedPreferencesNotifier.value == null) {
+      // SharedPreferences is not initialized yet -- delay update for when
+      // it is initialized
+      _updateToLatestValue =
+          () => _setValueAndWriteToSharedPreferences(newValue);
+    } else {
+      // Set value now, and write through to SharedPreferences
+      _setValueAndWriteToSharedPreferences(newValue);
+    }
+  }
+
+  void _readFromSharedPreferencesAndSetValue(T initialValue) {
+    final sharedPreferences = _sharedPreferencesNotifier.value!;
     super.value = switch (this) {
           PersistentValueNotifier<int?>() =>
-            _sharedPreferences!.getInt(sharedPreferencesKey) as T?,
+            sharedPreferences.getInt(sharedPreferencesKey) as T?,
           PersistentValueNotifier<bool?>() =>
-            _sharedPreferences!.getBool(sharedPreferencesKey) as T?,
+            sharedPreferences.getBool(sharedPreferencesKey) as T?,
           PersistentValueNotifier<double?>() =>
-            _sharedPreferences!.getDouble(sharedPreferencesKey) as T?,
+            sharedPreferences.getDouble(sharedPreferencesKey) as T?,
           PersistentValueNotifier<String?>() =>
-            _sharedPreferences!.getString(sharedPreferencesKey) as T?,
+            sharedPreferences.getString(sharedPreferencesKey) as T?,
           PersistentValueNotifier<List<String>?>() =>
-            _sharedPreferences!.getStringList(sharedPreferencesKey) as T?,
+            sharedPreferences.getStringList(sharedPreferencesKey) as T?,
           _ => throw Exception('Type parameter not supported'),
         } ??
         initialValue;
   }
 
-  @override
-  set value(T newValue) {
+  void _setValueAndWriteToSharedPreferences(T newValue) {
+    final sharedPreferences = _sharedPreferencesNotifier.value!;
     // Update the `ValueNotifier` superclass with the new value
     super.value = newValue;
     // Asynchronously write the new value through to SharedPreferences
-    unawaited(switch (newValue) {
-      null => _sharedPreferences!.remove(sharedPreferencesKey),
-      int() => _sharedPreferences!.setInt(sharedPreferencesKey, newValue),
-      bool() => _sharedPreferences!.setBool(sharedPreferencesKey, newValue),
-      double() => _sharedPreferences!.setDouble(sharedPreferencesKey, newValue),
-      String() => _sharedPreferences!.setString(sharedPreferencesKey, newValue),
+    (switch (newValue) {
+      null => sharedPreferences.remove(sharedPreferencesKey),
+      int() => sharedPreferences.setInt(sharedPreferencesKey, newValue),
+      bool() => sharedPreferences.setBool(sharedPreferencesKey, newValue),
+      double() => sharedPreferences.setDouble(sharedPreferencesKey, newValue),
+      String() => sharedPreferences.setString(sharedPreferencesKey, newValue),
       List<String>() =>
-        _sharedPreferences!.setStringList(sharedPreferencesKey, newValue),
+        sharedPreferences.setStringList(sharedPreferencesKey, newValue),
       _ =>
         // Should not happen, checked in constructor
         throw Exception('Type parameter not supported: $runtimeType'),
+    })
+        .then((success) {
+      if (!success) {
+        // Should not happen (I don't know when the platform backends could
+        // return false), but check anyway
+        throw Exception('Could not write value to SharedPreferences: '
+            '$sharedPreferencesKey = $newValue');
+      }
     });
   }
 }
